c537212b28229de52a427299e7090175822274ba
Nico Hagenburger <nico@hagenburger.net>
2010-08-14 22:03:56 +0200

    added missing engine integration (for staticmatic)

--- lib/lemonade/sass_extension.rb
+++ lib/lemonade/sass_extension.rb
@@ -5,4 +5,14 @@ module Sass
       Lemonade.generate_sprites if update_stylesheets_without_lemonade
     end
   end
+  class Engine
+    alias_method :render_without_lemonade, :render
+    def render
+      if result = render_without_lemonade
+        Lemonade::generate_sprites
+        result
+      end
+    end
+    alias_method :to_css, :render
+  end
 end
